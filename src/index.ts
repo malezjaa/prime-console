08655eca6c7245eb9b582be04fafd851eb3b5690
Krzysztof <66274761+malezjaa@users.noreply.github.com>
2023-10-27 22:58:32 +0200

    feat: add colors to export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { LoggerConfig } from "./types";
 import { ConsoleInstance } from "./instance";
+import { createColors } from "./utils";
 
 export const createLogger = (options: LoggerConfig = {}) => {
   return new ConsoleInstance(options);
 };
 
 export const logger = createLogger();
+let colors = { ...createColors(true) };
 
 export * from "./types";
 export { logColors, icons, strWidth, join } from "./utils";
+export { colors };
